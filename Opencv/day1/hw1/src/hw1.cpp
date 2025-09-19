6561f20f6c38e07be9736a4cf2e502ac4a55e5cc
moieboy9999 <61773236+moieboy9999@users.noreply.github.com>
2025-09-19 13:16:53 +0900

    Add files via upload

---
+++ Opencv/day1/hw1/src/hw1.cpp
@@ -0,0 +1,43 @@
+#include"../include/hw1/hw1.hpp"
+
+
+int main(){
+
+    obj row;
+    row.show("row");
+
+    obj ir;
+    ir.hsv();
+    ir.range(150,50,50,180,255,255,0,50,50,10,255,255);
+    ir.show("inrageR");
+
+    obj ig;
+    ig.hsv();
+    ig.range(30,100,150,70,255,255);
+    ig.show("inrageG");
+
+    obj ib;
+    ib.hsv();
+    ib.range(95,230,150,125,255,255);
+    ib.show("inrageB");
+
+    obj ibr;
+    ibr.hsv();
+    ibr.buler(51);
+    ibr.range(150,50,50,180,255,255,0,50,50,10,255,255);
+    ibr.show("inragebR");
+
+    obj ibg;
+    ibg.hsv();
+    ibg.buler(51);
+    ibg.range(30,100,150,70,255,255);
+    ibg.show("inragebG");
+
+    obj ibb;
+    ibb.hsv();
+    ibb.buler(51);
+    ibb.range(95,230,150,125,255,255);
+    ibb.show("inragebB");
+
+    waitKey(0);
+}
