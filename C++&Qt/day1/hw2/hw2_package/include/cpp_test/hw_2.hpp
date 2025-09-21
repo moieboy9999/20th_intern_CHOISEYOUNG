8e689bb97b85994bb1ef308620d9de2b992a7b82
moieboy9999 <61773236+moieboy9999@users.noreply.github.com>
2025-09-21 17:39:16 +0900

    Add files via upload

---
+++ C++&Qt/day1/hw2/hw2_package/include/cpp_test/hw_2.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+struct cold
+{
+    int X;
+    int Y;
+};
+
+class pross{
+    private:
+
+        float mid,mad;
+
+    public:
+        float input();
+        int muld(int a);
+        double sqrt(int a);
+
+};
