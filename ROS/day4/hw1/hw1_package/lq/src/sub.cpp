e9103c8d832235a8f2a5a07f3a26038e9d898965
moieboy9999 <61773236+moieboy9999@users.noreply.github.com>
2025-09-22 17:26:06 +0900

    Add files via upload

---
+++ ROS/day4/hw1/hw1_package/lq/src/sub.cpp
@@ -0,0 +1,58 @@
+
+#include <memory>
+#include <string>
+
+#include "lifecycle_msgs/msg/transition_event.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rcutils/logging_macros.h"
+#include "std_msgs/msg/string.hpp"
+
+class LifecycleListener : public rclcpp::Node
+{
+public:
+  explicit LifecycleListener(const std::string & node_name)
+  : Node(node_name)
+  {
+    rclcpp::QoS qos_profile(10);
+    sub_data_ = this->create_subscription<std_msgs::msg::String>(
+      "lifecycle_chatter", qos_profile,
+      std::bind(&LifecycleListener::data_callback, this, std::placeholders::_1));
+
+
+    sub_notification_ = this->create_subscription<lifecycle_msgs::msg::TransitionEvent>(
+      "/lc_talker/transition_event",qos_profile,
+      std::bind(&LifecycleListener::notification_callback, this, std::placeholders::_1));
+  }
+
+  void data_callback(std_msgs::msg::String::ConstSharedPtr msg)
+  {
+    RCLCPP_INFO(get_logger(), "data_callback: %s", msg->data.c_str());
+  }
+
+  void notification_callback(lifecycle_msgs::msg::TransitionEvent::ConstSharedPtr msg)
+  {
+    RCLCPP_INFO(
+      get_logger(), "notify callback: Transition from state %s to %s",
+      msg->start_state.label.c_str(), msg->goal_state.label.c_str());
+  }
+
+private:
+  std::shared_ptr<rclcpp::Subscription<std_msgs::msg::String>> sub_data_;
+  std::shared_ptr<rclcpp::Subscription<lifecycle_msgs::msg::TransitionEvent>>
+  sub_notification_;
+};
+
+int main(int argc, char ** argv)
+{
+
+  setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+
+  rclcpp::init(argc, argv);
+
+  auto lc_listener = std::make_shared<LifecycleListener>("lc_listener");
+  rclcpp::spin(lc_listener);
+
+  rclcpp::shutdown();
+
+  return 0;
+}
